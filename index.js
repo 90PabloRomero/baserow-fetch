d43dedecb3726ae9a7b2dcb99cc5ebe1a7834f2d
Pablo Romero <90pabloromero@gmail.com>
2022-05-12 23:00:31 -0300

    -search -filtrado -diseño responsive

--- index.js
+++ index.js
@@ -2,31 +2,14 @@
 //  Configuracion inicial
 ////////
 // noinspection CommaExpressionJS
-
 const TABLENUMBER = 63796
 
-// dom manipulation
-const newDiv = document.createElement('div');
-
-
-
 // constantes front - busqueda
-const searchForm = document.querySelector("form")
-const searchInput = searchForm.querySelector("input");
-const searchText = searchInput.value
+const inputIngresado = document.querySelector('#inputIngresado')
 // constantes front -resultados
-
-
-
-
 const resultsContainerFields = document.querySelector('#query_results_field')
 const resultsContainerRows = document.querySelector('#query_results_rows')
-
-
-
 // constantes fetch
-
-
 
 //funciones fetch
 const getAllDataFields = () => {
@@ -42,17 +25,17 @@ const getAllDataFields = () => {
     .then(response =>{
     const fields = response.data;
 
+      document.documentElement.style.setProperty('--colNum', fields.length+1);
     fields?.map(function (fields,i){
       return (
-        resultsContainerFields.appendChild(document.createElement('div')).textContent = fields.name
-    )})
+        resultsContainerFields.appendChild(document.createElement('th')).textContent = fields.name
+    )
+    })
 
     // console.log(fields)
     })
     .catch(error => console.log(error))
 };
-
-
 const getAllDataRows = () => {
   axios({
     method: "GET",
@@ -67,14 +50,57 @@ const getAllDataRows = () => {
       const losValuesDeRow = Object.values(rows)
       const returnRows = losValuesDeRow?.map(function (rows,i){
         delete(rows.order)
-        return(Object.values(rows).map(el => `<div>${el}</div>`).join(" "))
+        return(
+          // resultsContainerRows.append(newDiv),
+        Object.values(rows).map(el =>
+          `<td>${el}</td>`
+          // el
+        ).join('')
+
+        )
       })
-      console.log(returnRows)
-      returnRows.map((elem) => newDiv.innerHTML += elem)
-      resultsContainerRows.append(newDiv)
+      for (let i = 0; i < returnRows.length; i++) {
+
+        resultsContainerRows.innerHTML += '<tr class="row is-hidden">'+returnRows[i]+'</tr>'
+      }
+
+
     })
     .catch(error => console.log(error))
 };
 
 getAllDataFields();
 getAllDataRows();
+
+function liveSearch() {
+  const rowsResultsSearch = document.querySelectorAll('.row')
+
+  let search_query = document.getElementById("searchbox").value;
+  //Use innerText if all contents are visible
+  //Use textContent for including hidden elements
+  inputIngresado.innerHTML = (search_query)
+
+  for(let i = 0; i < rowsResultsSearch.length; i++){
+
+    if(rowsResultsSearch[i].textContent.toLowerCase()
+      .includes(search_query.toLowerCase())) {
+      rowsResultsSearch[i].classList.remove("is-hidden");
+
+    } else {
+      rowsResultsSearch[i].classList.add("is-hidden");
+    }
+  }
+
+
+}
+
+//A little delay
+let typingTimer;
+let typeInterval = 500;
+let searchInput = document.getElementById('searchbox');
+
+searchInput.addEventListener('keyup', () => {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(liveSearch, typeInterval);
+});
+
